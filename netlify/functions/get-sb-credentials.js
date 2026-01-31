7bfade7dcc7c1f8a1082718699387697022fd347
Paluss1122-1 <Paluss1122@gmail.com>
2026-01-31 14:41:44 +0100

    unnecessary

--- netlify/functions/get-sb-credentials.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/get-data.js
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
-
-serve(async (req) => {
-  const { data, error } = await supabase.from("users").select("*");
-  return new Response(JSON.stringify({ data, error }), {
-    headers: { "Content-Type": "application/json" },
-  });
-});
