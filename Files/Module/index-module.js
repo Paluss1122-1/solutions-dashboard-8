df268bb21df2c07cd4c2122bd01ffb2d066ebde5
Paluss1122-1 <Paluss1122@gmail.com>
2026-01-31 14:52:48 +0100

    .

--- Files/Module/index-module.js
+++ Files/Module/index-module.js
@@ -60,11 +60,11 @@ window.reporterror = reporterror;
 function getBrowserName() {
     const userAgent = navigator.userAgent;
 
+    if (userAgent.includes('Edge')) return 'Edge';
+    if (userAgent.includes('Opera')) return 'Opera';
     if (userAgent.includes('Firefox')) return 'Firefox';
     if (userAgent.includes('Chrome')) return 'Chrome';
     if (userAgent.includes('Safari') && !userAgent.includes('Chrome')) return 'Safari';
-    if (userAgent.includes('Edge')) return 'Edge';
-    if (userAgent.includes('Opera')) return 'Opera';
     if (userAgent.includes('Trident')) return 'Internet Explorer';
 
     return 'Unknown';
